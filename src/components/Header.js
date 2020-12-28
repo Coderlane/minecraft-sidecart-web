6871a18c9a1608c4be0292cc4b65d6b50f1e1646
Travis Lane <coderlane@gmail.com>
2020-12-28 11:31:00 -0500

    Use bootstrap for header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import { auth } from '../services/firebase';
 
 function Header() {
   return (
     <header>
-      <nav className="navbar navbar-expand-sm fixed-top navbar-light bg-light">
-        <Link className="navbar-brand" to="/">Sidecart</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          {auth().currentUser
-            ? (
-              <div className="navbar-nav">
-                <Link className="nav-item nav-link" to="/servers">Servers</Link>
-                <button className="btn btn-primary" type="button" onClick={() => auth().signOut()}>Logout</button>
-              </div>
-            )
-            : (
-              <div className="navbar-nav">
-                <Link className="nav-item nav-link" to="/login">Sign In</Link>
-              </div>
-            )}
-        </div>
-      </nav>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Sidecart</Navbar.Brand>
+        {auth().currentUser
+          ? (
+            <>
+              <Nav className="mr-auto">
+                <Nav.Link href="/servers">Servers</Nav.Link>
+              </Nav>
+              <Form inline>
+                <Button onClick={() => auth().signOut()}>Sign Out</Button>
+              </Form>
+            </>
+          )
+          : (
+            <>
+              <Nav className="mr-auto" />
+              <Nav>
+                <Nav.Link href="/login">Sign In</Nav.Link>
+              </Nav>
+            </>
+          )}
+      </Navbar>
     </header>
   );
 }
