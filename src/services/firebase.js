27ad9325d47377cac9cec4aa160450e09274bf36
Travis Lane <coderlane@gmail.com>
2021-01-06 21:32:38 -0500

    fixup

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const { auth } = firebase;
-const { db } = firebase.firestore();
+const db = firebase.firestore();
 
 export { auth, db };
