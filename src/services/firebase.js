f6b161ce20c49b1636cb991d500e59734ca10237
Travis Lane <coderlane@gmail.com>
2021-01-06 21:15:03 -0500

    Use Firestore instead of Realtime Database

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,6 +14,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const { auth } = firebase;
-const { db } = firebase.database();
+const { db } = firebase.firestore();
 
 export { auth, db };
